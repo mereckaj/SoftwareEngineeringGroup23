b81cd44499912d578f54fbb618fb69a2ea44298b
Julius <mereckaj@tcd.ie>
2015-02-20 18:29:40 +0000

    Added buttons, removed alerts, added key listener Added buttons to select x and y coordinates Changed previous button to now make the guess Added keylistener for Enter/Return which in turns makes a guess Added last guess locator, the dot at last x and y coordinates now shows up in green

--- rhino/rhino.js
+++ rhino/rhino.js
@@ -3,6 +3,12 @@ var gridSizeY = 5;
 var rhino;
 var grid;
 var realDots;
+var lastGuessColour = "#33FF00";
+var normalColour = "#000000";
+var xCoord;
+var yCoord;
+var lastx;
+var laxty;
 
 /*****************************************
   *             Rhino Object             *
@@ -14,8 +20,7 @@ function Rhino(){
   var yPos = Math.floor(Math.random() * (gridSizeY));  //generate a random Y position for the rhino in the 2D array
 
   rhino.sayPos = function(){    //alerts user with the co ordinates of the rhino;
-    alert(xPos);
-    alert(yPos);
+    alert(xPos+" " + yPos);
   };
 
   rhino.move = function(){    //moves the rhino to new co-ordinates
@@ -57,46 +62,55 @@ function City(){
 
 
 var main = function(){
-  alert("script is running!");
   rhino = new Rhino();
-  rhino.sayPos();
+  // rhino.sayPos();
   grid = new City();
 
   var guessButton = document.getElementById('makeGuess');     //gets the button by ID *allows us to check if the button is pressed*
   guessButton.addEventListener('click', makeGuess, false);    //when the button is pressed the makeGuess() function is called
   guessButton.disabled = false;
-
-
+  window.onkeydown = function (e) {
+    var code = e.keyCode ? e.keyCode : e.which;
+    if (code === 13 ) { //up key
+      makeGuess();
+    }
+  };
 }
 
 function makeGuess(){
   var userInput;
-  var xCoord;
-  var yCoord;
+  lastx = xCoord;
+  lasty = yCoord;
+  xCoord =  document.getElementById('x_val').value;
+  yCoord =  document.getElementById('y_val').value;
+  //Set colour of last guess
+  $svg = $("#svg_gird");
+  $("#"+xCoord+""+yCoord, $svg).attr('style', "fill:"+lastGuessColour);
+  $("#"+lastx+""+lasty, $svg).attr('style', "fill:"+normalColour);
 
-  while(true){
-    xCoord = getNumber("Enter an x co-ordinate!");
-    if(!isNaN(xCoord) && xCoord != null){
-      if(xCoord < 0){
-        alert("x co-ordinate is out of range! range:0-" + (gridSizeX-1));
-      }else if(xCoord > gridSizeX-1){
-        alert("x co-ordinate is out of range! range:0-" + (gridSizeX-1));
-      }
-      break;
-    }
-  }
+  // while(true){
+  //   xCoord = getNumber("Enter an x co-ordinate!");
+  //   if(!isNaN(xCoord) && xCoord != null){
+  //     if(xCoord < 0){
+  //       alert("x co-ordinate is out of range! range:0-" + (gridSizeX-1));
+  //     }else if(xCoord > gridSizeX-1){
+  //       alert("x co-ordinate is out of range! range:0-" + (gridSizeX-1));
+  //     }
+  //     break;
+  //   }
+  // }
 
-  while(true){
-    yCoord = getNumber("Enter a y co-ordinate!");
-    if(!isNaN(yCoord) && yCoord != null){
-      if(yCoord < 0){
-        alert("y co-ordinate is out of range! range:0-" + (gridSizeY-1));
-      }else if(yCoord > gridSizeY-1){
-        alert("y co-ordinate is out of range! range:0-" + (gridSizeY-1));
-      }
-      break;
-    }
-  }
+  // while(true){
+  //   yCoord = getNumber("Enter a y co-ordinate!");
+  //   if(!isNaN(yCoord) && yCoord != null){
+  //     if(yCoord < 0){
+  //       alert("y co-ordinate is out of range! range:0-" + (gridSizeY-1));
+  //     }else if(yCoord > gridSizeY-1){
+  //       alert("y co-ordinate is out of range! range:0-" + (gridSizeY-1));
+  //     }
+  //     break;
+  //   }
+  // }
 
   if(rhino.checkPos(xCoord, yCoord) === true){
     alert("You have found the Rhino");
